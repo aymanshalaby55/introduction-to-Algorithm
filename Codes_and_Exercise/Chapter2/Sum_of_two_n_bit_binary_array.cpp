d04125a0f793a68df5ff5bb3ae750614dee80f58
ayman <aymanshalaby539@gmail.com>
2023-02-06 07:22:13 +0200

    Sum of two n-bit binary numbers

---
+++ Codes_and_Exercise/Chapter2/Sum_of_two_n_bit_binary_array.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+    int carry = 0;
+    vector<int> c(n + 1);
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int value = a[i] + b[i] + carry;
+        if (value > 1)
+        {
+            if (value == 2)
+            {
+                c[i + 1] = 0;
+                carry = 1;
+            }
+            else
+            {
+                c[i + 1] = 1;
+                carry = 1;
+            }
+        }
+        else
+            c[i + 1] = value;
+    }
+    c[0] = carry;
+    for (auto i : c)
+        cout << i << " ";
+}
